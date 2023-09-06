8aec89a9556280bdc9177ee5b7f9d534eea25cde
Stuart Morrison <stuart.morrison@kcl.ac.uk>
2023-09-06 22:39:59 +0100

    fix: assign opts and rclone_opts

--- manifests/service.pp
+++ manifests/service.pp
@@ -84,13 +84,10 @@ define rclone::service (
 
   if $active {
     if $opts == undef {
-      $rclone_opts = ''
-    }
-    else {
-      $rclone_opts = $opts
+      $opts = ''
     }
     if $conf != undef {
-      $rclone_opts = "${rclone_opts} --config=/etc/rclone/${name}_rclone.conf"
+      $rclone_opts = "${opts} --config=/etc/rclone/${name}_rclone.conf"
       rclone::config { $name:
         group => $group,
         conf  => $conf,
