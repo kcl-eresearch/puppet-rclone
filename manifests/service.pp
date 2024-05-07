9c54688a3b5eb7d07d48f407a336f29f0a028a14
Xand Meaden <xand.meaden@kcl.ac.uk>
2024-05-07 15:38:45 +0100

    feat: make slack messages optional

--- manifests/service.pp
+++ manifests/service.pp
@@ -70,6 +70,7 @@ define rclone::service (
   Optional[Array[String]] $pre_rclone      = undef,
   Optional[Array[String]] $post_rclone     = undef,
   Optional[Stdlib::HTTPUrl] $http_proxy    = undef,
+  Boolean $slack_msg = true,
 ) {
   if $active {
     if $conf != undef {
